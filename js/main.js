be706c189b14180463a90d06fbdc02a63e30ffb6
Irina Brennen <irinabrennen@twc.com>
2017-11-13 07:06:22 -0600

    Added stop game functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 let buttonPressSeries = new ButtonPressSeries();
 let roundManager = new RoundManager(buttonPressSeries,0,0,false);
 
+var showStopButton = () => document.getElementById("stop-button").style.display = "inline-block";
+
+var hideStopButton = () => document.getElementById("stop-button").style.display = "none";
+
 var handleUserButtonPress = (event) => {  
     
         roundManager.handlePlayerInput(event.target);
@@ -11,9 +15,15 @@ var start = (event) => {
     event.target.setAttribute("value","Restart");
     roundManager.reset();
     roundManager.readyForNextRound = true;
+    showStopButton();
     
     }
 
+var stop = () => {
+    roundManager.readyForNextRound = false;
+    hideStopButton();
+
+};
 setInterval( () => { 
     if (roundManager.readyForNextRound)
         roundManager.nextRound() }, 3000);
